68ca6463f1ca68889cd2a202a7727b2c0bd62e52
mhlan <33137338+mhlan@users.noreply.github.com>
2018-04-17 16:53:36 -0700

    Moved quotes array into script.js, disconnected quotes.js temporarily.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,47 @@
 // document
 //   .getElementById("loadQuote")
 //   .addEventListener("click", printQuote, false);
+const quotes = [
+  {
+    quote:
+      "Fairytales don't teach children that monsters exist, they already know that monsters exist. Fairy tales teach children that monsters can be killed.",
+    source: "G.K. Chesterton",
+    citation: "",
+    year: ""
+  },
+  {
+    quote:
+      "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
+    source: "Douglas Adams",
+    citation: "Mostly Harmless",
+    year: "1992"
+  },
+  {
+    quote: "Just because you are offended does not mean you're right.",
+    source: "Ricky Gervais",
+    citation: "Twitter",
+    year: "2013"
+  },
+  {
+    quote:
+      "The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.",
+    source: "Bertrand Russell",
+    citation: "A Word a Day",
+    year: "2002"
+  },
+  {
+    quote:
+      "There are two possibilities: either we are alone in the universe or we are not. Both are equally terrifying",
+    source: "Arthur C. Clarke",
+    citation:
+      "Visions: How Science Will Revolutionize the Twenty-First Century",
+    year: "1999"
+  }
+];
+//uses a pseudo random number to pull a random quote from "quotes"
+function getRandomQuote() {
+  let RNG = Math.floor(Math.random() * 6);
+  return quotes[RNG];
+}
+
+getRandomQuote();
