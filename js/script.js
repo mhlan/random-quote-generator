8b39b0c3eab7edca9325c86ebd6be44201e91b12
mhlan <33137338+mhlan@users.noreply.github.com>
2018-04-17 13:45:38 -0700

    Commented out starter JS for now.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
+// when user clicks anywhere on the button, the 'printQuote' function is called
+// document
+//   .getElementById("loadQuote")
+//   .addEventListener("click", printQuote, false);
