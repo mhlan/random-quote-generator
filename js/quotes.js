53e30c4545c0eaf687a3bf8d3ca8bb8c8374ecd8
mhlan <33137338+mhlan@users.noreply.github.com>
2018-04-17 16:16:56 -0700

    Completed data within each object.

--- js/quotes.js
+++ js/quotes.js
@@ -2,35 +2,36 @@ const quotes = [
   {
     quote:
       "Fairytales don't teach children that monsters exist, they already know that monsters exist. Fairy tales teach children that monsters can be killed.",
-    source: "G. K. Chesterton",
-    citation: "blank",
-    year: "blank"
+    source: "G.K. Chesterton",
+    citation: "",
+    year: ""
   },
   {
     quote:
       "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
     source: "Douglas Adams",
-    citation: "blank",
-    year: "blank"
+    citation: "Mostly Harmless",
+    year: "1992"
   },
   {
     quote: "Just because you are offended does not mean you're right.",
     source: "Ricky Gervais",
-    citation: "blank",
-    year: "blank"
+    citation: "Twitter",
+    year: "2013"
   },
   {
     quote:
       "The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.",
     source: "Bertrand Russell",
-    citation: "blank",
-    year: "blank"
+    citation: "A Word a Day",
+    year: "2002"
   },
   {
     quote:
       "There are two possibilities: either we are alone in the universe or we are not. Both are equally terrifying",
     source: "Arthur C. Clarke",
-    citation: "blank",
-    year: "blank"
+    citation:
+      "Visions: How Science Will Revolutionize the Twenty-First Century",
+    year: "1999"
   }
 ];
