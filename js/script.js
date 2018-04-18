323f67a06ddc57ecef6ac70bb82b73ae6c9f6db2
mhlan <33137338+mhlan@users.noreply.github.com>
2018-04-18 10:47:32 -0700

    Fixed line length, added random bg color function

--- js/script.js
+++ js/script.js
@@ -7,7 +7,9 @@ document
 const quotes = [
   {
     quote:
-      "Fairytales don't teach children that monsters exist, they already know that monsters exist. Fairy tales teach children that monsters can be killed.",
+      "Fairytales don't teach children that monsters exist, they already \
+      know that monsters exist. Fairy tales teach children that monsters \
+      can be killed.",
     source: "G.K. Chesterton",
     citation: "",
     year: NaN,
@@ -15,7 +17,9 @@ const quotes = [
   },
   {
     quote:
-      "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
+      "A common mistake that people make when trying to design something \
+      completely foolproof is to underestimate the ingenuity of complete \
+      fools.",
     source: "Douglas Adams",
     citation: "Mostly Harmless",
     year: 1992,
@@ -30,7 +34,8 @@ const quotes = [
   },
   {
     quote:
-      "The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.",
+      "The whole problem with the world is that fools and fanatics are \
+      always so certain of themselves, and wiser people so full of doubts.",
     source: "Bertrand Russell",
     citation: "A Word a Day",
     year: 2002,
@@ -38,7 +43,8 @@ const quotes = [
   },
   {
     quote:
-      "There are two possibilities: either we are alone in the universe or we are not. Both are equally terrifying",
+      "There are two possibilities: either we are alone in the universe or \
+      we are not. Both are equally terrifying",
     source: "Arthur C. Clarke",
     citation:
       "Visions: How Science Will Revolutionize the Twenty-First Century",
@@ -46,13 +52,14 @@ const quotes = [
     tags: "Science"
   }
 ];
-//uses a pseudo random number to pull a random quote from the "quotes" array
+//generate a random number to pull a random quote from the "quotes" array
 function getRandomQuote() {
   let RNG = Math.floor(Math.random() * 5);
   return quotes[RNG];
 }
 //constructs HTML with random quote as a string and displays to page
-//if the values in citation/year are empty strings, doesn't generate corresponding HTML
+//if the values in citation/year don't pass check, doesn't generate...
+//...corresponding HTML
 function printQuote() {
   let randomQuote = getRandomQuote();
   let HTML = "<p class='quote'>" + randomQuote.quote + "</p>";
@@ -69,4 +76,13 @@ function printQuote() {
     randomQuote.tags.toLowerCase() +
     "</span>.</p>";
   document.getElementById("quote-box").innerHTML = HTML;
+  setBgColor();
+}
+//generates random color and sets it to background
+function setBgColor() {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  document.body.style.background = bgColor;
 }
