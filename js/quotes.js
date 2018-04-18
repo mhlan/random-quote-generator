71b3f886e0a63df9c7416ab27b27ec65fa03e950
mhlan <33137338+mhlan@users.noreply.github.com>
2018-04-18 12:21:45 -0700

    Moved quotes array back to quotes.js, expanded array, housecleaning.

--- js/quotes.js
+++ js/quotes.js
@@ -1,37 +1,90 @@
+//dataset for each quote, their author, the citation and year if available...
+//...and corresponding tag
 const quotes = [
   {
     quote:
-      "Fairytales don't teach children that monsters exist, they already know that monsters exist. Fairy tales teach children that monsters can be killed.",
+      "Fairytales don't teach children that monsters exist, they already \
+      know that monsters exist. Fairy tales teach children that monsters \
+      can be killed.",
     source: "G.K. Chesterton",
     citation: "",
-    year: ""
+    year: NaN,
+    tags: "Literature"
   },
   {
     quote:
-      "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
+      "A common mistake that people make when trying to design something \
+      completely foolproof is to underestimate the ingenuity of complete \
+      fools.",
     source: "Douglas Adams",
     citation: "Mostly Harmless",
-    year: "1992"
+    year: 1992,
+    tags: "Humor"
   },
   {
     quote: "Just because you are offended does not mean you're right.",
     source: "Ricky Gervais",
     citation: "Twitter",
-    year: "2013"
+    year: 2013,
+    tags: "Humor"
   },
   {
     quote:
-      "The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.",
+      "The whole problem with the world is that fools and fanatics are \
+      always so certain of themselves, and wiser people so full of doubts.",
     source: "Bertrand Russell",
     citation: "A Word a Day",
-    year: "2002"
+    year: 2002,
+    tags: "Philosophy"
   },
   {
     quote:
-      "There are two possibilities: either we are alone in the universe or we are not. Both are equally terrifying",
+      "There are two possibilities: either we are alone in the universe or \
+      we are not. Both are equally terrifying",
     source: "Arthur C. Clarke",
     citation:
       "Visions: How Science Will Revolutionize the Twenty-First Century",
-    year: "1999"
+    year: 1999,
+    tags: "Science"
+  },
+  {
+    quote:
+      "You know you’re in love when you can’t fall asleep because \
+    reality is finally better than your dreams.",
+    source: "Dr. Seus",
+    citation: "",
+    year: 0,
+    tags: "Life"
+  },
+  {
+    quote:
+      "It is hard to fail, but it is worse never to have tried to \
+    succeed.",
+    source: "Theodore Roosevelt",
+    citation: "Speech before the Hamilton Club, Chicago",
+    year: 1899,
+    tags: "Inspirational"
+  },
+  {
+    quote: "Love is a serious mental disease.",
+    source: "Plato",
+    citation: "Phaedrus",
+    year: 370,
+    tags: "Philosophy"
+  },
+  {
+    quote:
+      "Only put off until tomorrow what you are willing to die having left undone.",
+    source: "Pablo Picasso",
+    citation: "",
+    year: 0,
+    tags: "Inspirational"
+  },
+  {
+    quote: "The planet is fine. The people are fucked.",
+    source: "George Carlin",
+    citation: "",
+    year: 0,
+    tags: "Funny"
   }
 ];
--- js/script.js
+++ js/script.js
@@ -3,58 +3,9 @@
 document
   .getElementById("loadQuote")
   .addEventListener("click", printQuote, false);
-//dataset for each quote, their author, and citation and year if available
-const quotes = [
-  {
-    quote:
-      "Fairytales don't teach children that monsters exist, they already \
-      know that monsters exist. Fairy tales teach children that monsters \
-      can be killed.",
-    source: "G.K. Chesterton",
-    citation: "",
-    year: NaN,
-    tags: "Literature"
-  },
-  {
-    quote:
-      "A common mistake that people make when trying to design something \
-      completely foolproof is to underestimate the ingenuity of complete \
-      fools.",
-    source: "Douglas Adams",
-    citation: "Mostly Harmless",
-    year: 1992,
-    tags: "Humor"
-  },
-  {
-    quote: "Just because you are offended does not mean you're right.",
-    source: "Ricky Gervais",
-    citation: "Twitter",
-    year: 2013,
-    tags: "Humor"
-  },
-  {
-    quote:
-      "The whole problem with the world is that fools and fanatics are \
-      always so certain of themselves, and wiser people so full of doubts.",
-    source: "Bertrand Russell",
-    citation: "A Word a Day",
-    year: 2002,
-    tags: "Philosophy"
-  },
-  {
-    quote:
-      "There are two possibilities: either we are alone in the universe or \
-      we are not. Both are equally terrifying",
-    source: "Arthur C. Clarke",
-    citation:
-      "Visions: How Science Will Revolutionize the Twenty-First Century",
-    year: 1999,
-    tags: "Science"
-  }
-];
 //generate a random number to pull a random quote from the "quotes" array
 function getRandomQuote() {
-  let RNG = Math.floor(Math.random() * 5);
+  let RNG = Math.floor(Math.random() * 10);
   return quotes[RNG];
 }
 //constructs HTML with random quote as a string and displays to page
@@ -85,9 +36,11 @@ function setBgColor() {
   let z = Math.floor(Math.random() * 256);
   let bgColor = "rgb(" + x + "," + y + "," + z + ")";
   document.body.style.background = bgColor;
+  document.getElementById("loadQuote").style.background = bgColor;
 }
-
+//sets printQuote on a 30 second timer
 function timer() {
   window.setInterval(printQuote, 30000);
 }
+
 timer();
